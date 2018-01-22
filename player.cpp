ab80d40768eaabed2033fee1e1f08ec12d36edba
Joel <haystackjma@gmail.com>
2018-01-22 12:09:58 -0800

    Added in game instructions

--- player.cpp
+++ player.cpp
@@ -11,5 +11,7 @@ void Player::keyPressEvent(QKeyEvent *event)
         setPos(x(),y()-10);
     else if(event->key() == Qt::Key_Down && y()+30 < 290 )
         setPos(x(),y()+10);
+    else if(event->key() == Qt::Key_F1)
+        printf("This is the game of Pong\nThe game plays similarly to the classic game.\nMove up or down using the directional arrow keys up and down.\nPress F1 to have instructions sent to the screen.\n");
 }
 
